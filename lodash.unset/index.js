da8af5c1759ea3faea15f147dbc960d5c4d39ea1
John-David Dalton <john.david.dalton@gmail.com>
2016-02-16 00:58:24 -0800

    Bump to v4.3.1.

--- lodash.unset/index.js
+++ lodash.unset/index.js
@@ -1,5 +1,5 @@
 /**
- * lodash 4.3.0 (Custom Build) <https://lodash.com/>
+ * lodash 4.3.1 (Custom Build) <https://lodash.com/>
  * Build: `lodash modularize exports="npm" -o ./`
  * Copyright jQuery Foundation and other contributors <https://jquery.org/>
  * Released under MIT license <https://lodash.com/license>
@@ -47,7 +47,8 @@ var objectProto = Object.prototype;
 var hasOwnProperty = objectProto.hasOwnProperty;
 
 /**
- * Used to resolve the [`toStringTag`](http://ecma-international.org/ecma-262/6.0/#sec-object.prototype.tostring)
+ * Used to resolve the
+ * [`toStringTag`](http://ecma-international.org/ecma-262/6.0/#sec-object.prototype.tostring)
  * of values.
  */
 var objectToString = objectProto.toString;
@@ -167,29 +168,25 @@ function getPrototype(value) {
  * @returns {boolean} Returns `true` if `path` exists, else `false`.
  */
 function hasPath(object, path, hasFunc) {
-  if (object == null) {
-    return false;
-  }
-  var result = hasFunc(object, path);
-  if (!result && !isKey(path)) {
-    path = baseCastPath(path);
+  path = isKey(path, object) ? [path] : baseCastPath(path);
 
-    var index = -1,
-        length = path.length;
+  var result,
+      index = -1,
+      length = path.length;
 
-    while (object != null && ++index < length) {
-      var key = path[index];
-      if (!(result = hasFunc(object, key))) {
-        break;
-      }
-      object = object[key];
+  while (++index < length) {
+    var key = path[index];
+    if (!(result = object != null && hasFunc(object, key))) {
+      break;
     }
+    object = object[key];
   }
-  var length = object ? object.length : undefined;
-  return result || (
-    !!length && isLength(length) && isIndex(path, length) &&
-    (isArray(object) || isString(object) || isArguments(object))
-  );
+  if (result) {
+    return result;
+  }
+  var length = object ? object.length : 0;
+  return !!length && isLength(length) && isIndex(key, length) &&
+    (isArray(object) || isString(object) || isArguments(object));
 }
 
 /**
@@ -409,8 +406,9 @@ function isLength(value) {
 }
 
 /**
- * Checks if `value` is the [language type](https://es5.github.io/#x8) of `Object`.
- * (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
+ * Checks if `value` is the
+ * [language type](http://www.ecma-international.org/ecma-262/6.0/#sec-ecmascript-language-types)
+ * of `Object`. (e.g. arrays, functions, objects, regexes, `new Number(0)`, and `new String('')`)
  *
  * @static
  * @memberOf _
@@ -523,23 +521,23 @@ function isSymbol(value) {
  * @returns {boolean} Returns `true` if `path` exists, else `false`.
  * @example
  *
- * var object = { 'a': { 'b': { 'c': 3 } } };
- * var other = _.create({ 'a': _.create({ 'b': _.create({ 'c': 3 }) }) });
+ * var object = { 'a': { 'b': 2 } };
+ * var other = _.create({ 'a': _.create({ 'b': 2 }) });
  *
  * _.has(object, 'a');
  * // => true
  *
- * _.has(object, 'a.b.c');
+ * _.has(object, 'a.b');
  * // => true
  *
- * _.has(object, ['a', 'b', 'c']);
+ * _.has(object, ['a', 'b']);
  * // => true
  *
  * _.has(other, 'a');
  * // => false
  */
 function has(object, path) {
-  return hasPath(object, path, baseHas);
+  return object != null && hasPath(object, path, baseHas);
 }
 
 /**
@@ -563,7 +561,7 @@ function has(object, path) {
  * console.log(object);
  * // => { 'a': [{ 'b': {} }] };
  *
- * _.unset(object, 'a[0].b.c');
+ * _.unset(object, ['a', '0', 'b', 'c']);
  * // => true
  *
  * console.log(object);
